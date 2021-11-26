8676463df4a60332e9a92c5be71d79954a9c0cfc
92257852915 <artur.santos@serpro.gov.br>
2021-11-26 13:09:55 -0300

    Pegar o objeto que está dentro de outra contains, a diferença entre is e contain. Incluidas as assertivas para o body de teste incluirPet completado o Post

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -3,33 +3,53 @@ package petstore;
 
 import org.testng.annotations.Test;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static io.restassured.RestAssured.given;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+
+// Classe
 public class Pet  {
+    //atributos
     String uri = "https://petstore.swagger.io/v2/pet";
 
+    //métodos e funções
     public String lerJson(String caminhoJson) throws IOException {
         return new String(Files.readAllBytes(Paths.get(caminhoJson))) ;
 
     }
+
+    //incluir -create - post
     @Test  //identifica o método ou função como um teste para TestNG
     public void incluirPet() throws IOException {
         String jsonBody = lerJson("db/pet1.json");
 
-        given()
-                .contentType("application/json")
+        //sintaxe gherkin
+        //dado - quando - então
+        //given- when - then
+
+        given()//dado
+                .contentType("application/json")//comum em api rest - os mais antigos text/xml
                 .log().all()
                 .body(jsonBody)
-        .when()
+        .when()//quando
                 .post(uri)
-        .then()
+        .then()//então
                 .log().all()
                 .statusCode(200)
+                .body("name", is("snoopy"))
+                .body("status", is("available"))
+                //.body("category.id", contains("1"))
+                //.body("category.id", is("1"))
+                .body("category.name", is("dog"))
+                .body("tags.name", contains("sta"))
+
+
         ;
     }
 }
